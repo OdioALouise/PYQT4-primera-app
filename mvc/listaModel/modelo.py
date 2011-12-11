a518f47763318c9701feb6597d7ccc51f4353838
Sebastian <sebastiandaloia@gmail.com>
2011-12-11 09:40:58 -0300

    modelo andando

--- mvc/listaModel/modelo.py
+++ mvc/listaModel/modelo.py
@@ -1,14 +1,32 @@
 from PyQt4 import QtCore, QtGui
 import sys
 
+
+class equipo(object):
+    def __init__(self, nombre, asociacion):
+        self.nombre = nombre
+        self.asociacion = asociacion
+    """
+    def __init__(self):
+        self.nombre = "Natalia Natalia"
+        self.asociacion = "Natalia Natalia"
+    """
+        
+
 class modelo(QtCore.QAbstractItemModel):
 
-    def __init__(self):
-        atributo1 = "atributo11"
-        atributo2 = "atributo22"
-        lista = []
-        lista.append(atributo1)
-        lista.append(atributo2)
+    def __init__(self, parent = None):
+        super(modelo, self).__init__(parent)
+
+        self.lista = []
+
+        for nombre, asociacion in (
+            ("Rampla Juniors", "AUF"),
+            ("Los Sauces", "OFI"),
+            ("Tacuarembo FC", "AUF")):
+            self.lista.append(equipo(nombre, asociacion))
+
+
         
     def flags(self, index):
         return QtCore.Qt.ItemIsEditable | QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
@@ -16,17 +34,27 @@ class modelo(QtCore.QAbstractItemModel):
     def rowCount(self, parent = QtCore.QModelIndex()):
         return len(self.lista)
 
+    def columnCount(self, parent = QtCore.QModelIndex()):
+        return 0
 
     def data(self, index, role):
 
         if role==QtCore.Qt.ToolTipRole:
-            return "Codigo Hex: " + self.lista[index.row()].name()
+
+            valor = self.lista[index.row()]
+                       
+            return "Codigo Hex: " + QtCore.QVariant(valor.asociacion).toString()
 
        
         if role == QtCore.Qt.DisplayRole:
             row = index.row()
+          
             value = self.lista[row]
-            return value.name()            
+
+            return QtCore.QVariant(value.nombre)
+
+    def index(row, column, parent = QtCore.QModelIndex()):
+        return QtCore.QAbstractItemModel.createIndex(row, column, object = 0)
 
 
 class delegado(QtGui.QAbstractItemDelegate):
@@ -76,20 +104,28 @@ if __name__ == "__main__":
 
     app = QtGui.QApplication(sys.argv)
 
-    data= QtCore.QStringList()
-    data << "uno" << "dos"
-    
     m = modelo()
 
+
+    i = 0
+    
+    for i in range(m.rowCount()):
+
+       indice = m.index(i, m.columnCount()) 
+
+       valor = m.data(indice, role = QtCore.Qt.DisplayRole)
+
+       print valor.toString()
+    
     """
     dialog = dialogo()
     dialog.open()
     """
-    vista1=QtGui.QListView()
+    vista1 = QtGui.QTreeView()
     
     vista1.show()
 
-    vista1.setModel(data)
+    vista1.setModel(m)
 
 
     sys.exit(app.exec_())
